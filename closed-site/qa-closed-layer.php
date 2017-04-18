ba2d08fd18cc85b6995be8c7705b93bd0d4a4da9
Jackson Siro <smataweb@gmail.com>
2017-04-18 11:23:14 +0300

    Update qa-closed-layer.php

--- closed-site/qa-closed-layer.php
+++ closed-site/qa-closed-layer.php
@@ -9,134 +9,238 @@
 	Plugin Author URI: http://github.com/JackSiro
 	Plugin License: GPLv3
 	Plugin Minimum Question2Answer Version: 1.7
-	Plugin Update Check URI:
+	Plugin Update Check URI: https://github.com/JackSiro/Q2A-Closed-Site-Plugin/master/closed-site/qa-plugin.php 
 
 */
-require_once QA_INCLUDE_DIR.'qa-theme-base.php';
 
-	class qa_html_theme_layer extends qa_html_theme_base 
-	{	
-		private $fixed_topbar = false;
+	require_once QA_INCLUDE_DIR.'db/admin.php';
+	require_once QA_INCLUDE_DIR.'db/maxima.php';
+	require_once QA_INCLUDE_DIR.'db/selects.php';
+	require_once QA_INCLUDE_DIR.'app/options.php';
+	require_once QA_INCLUDE_DIR.'app/admin.php';
+
+class qa_html_theme_layer extends qa_html_theme_base {
+	
+	var $plugin_directory;
+	var $plugin_url;
+	function qa_html_theme_layer($template, $content, $rooturl, $request)
+	{
+		global $qa_layers;
+		$this->plugin_directory = $qa_layers['Closed Site Settings']['directory'];
+		$this->plugin_url = $qa_layers['Closed Site Settings']['urltoroot'];
+		qa_html_theme_base::qa_html_theme_base($template, $content, $rooturl, $request);
+	}
+	
+	function nav_list($navigation, $class, $level=null)
+	{
+		if(qa_opt('closed_site_allow') && $this->template=='admin') {
+			if ($class == 'nav-sub') {
+				$navigation['closedsite'] = array(
+					'label' => qa_lang('qa_closed_lang/closed_site'),
+					'url' => qa_path_html('admin/closedsite'),
+				);
+			}
 			
-		function head_css() {
-			qa_html_theme_base::head_css();
-			if (qa_opt('closed_site_allow')) {
-				if (!qa_is_logged_in()) {
-					$css = array('<style>');
-					$this->output('<link href="' . qa_path_to_root().'qa-plugin/closed-site/qa-style.css" type="text/css" rel="stylesheet"/>');
-					if (qa_opt('closed_site_background_allow')) {
-						$css[] = 'body{background:url(nairobi.jpg) no-repeat fixed center 100% !important;}';
-						
-					} else {
-						$css[] = '.qa-closed-left{background: #000;}';	
-						$css[] = '.qa-closed-right{border: 1px solid #000;}';	
-					}
-					$css[] = '</style>';
-					$this->output_array($css);
-				}
+			if($this->request == 'admin/closedsite') {
+				$newnav = qa_admin_sub_navigation();
+				$navigation = array_merge($newnav, $navigation);
+				$navigation['admin']['closedsite'] = true; 
 			}
 		}
 		
-		
-		function html()
-		{
-			
-			$this->output('<html>');		
-			$this->head();			
-			$this->output('<body>');
-			
-			if (qa_opt('closed_site_allow')) {
-				if (!qa_is_logged_in()) {
-					if (isset($this->content['navigation']['user']['login']) && !QA_FINAL_EXTERNAL_USERS) {
-						$login=@$this->content['navigation']['user']['login'];
-						if (qa_request_part(0) == 'register') $this->body();					
-						else $this->qa_closed_page($login);
-						unset($this->content['navigation']['user']['login']);
-					}
-				}
-				else $this->body();
-				
-			} else $this->body();
-			
-			$this->output('</body>');
-			$this->output('</html>');
-		}
-		public function qa_closed_header()
-		{
-			$class = $this->fixed_topbar ? ' fixed' : '';
-			$this->output('<div id="qam-topbar" class="clearfix' . $class . '">');
-			$this->output('<div class="qam-main-nav-wrapper clearfix">');
-			$this->nav_user_search();
-			$this->logo();
-			$this->output('</div>');
-		}
-		
-		function qa_closed_page($login)
-		{
-			$this->body_prefix();
-			$this->notices();			
-			$this->qa_closed_header();
-
-			$this->output('<div class="qa-body-wrapper">', '');
-			$this->output(qa_opt('closed_site_html_top'), '<!-- Html Top -->');
-			
-			$this->output('<div class="qa-main-wrapper">', '');
-			
-			$this->qa_closed_right();
-			$this->output('<div class="qa-closed-right">');
-			
-			$this->qa_closed_left($login);
-			$this->output('</div>', '');
-			
-			$this->output('</div>');
-			$this->output(qa_opt('closed_site_html_bottom'), '<!-- Html Bottom -->');
-
-			$this->output('</div>');
-
-			$this->footer();
-			$this->body_suffix();
-		}
-		
-		function qa_closed_right()
-		{
-			$page_title = qa_opt('closed_site_page_title') ? qa_opt('closed_site_page_title') : qa_lang('qa_closed_lang/default_page_title');
-			$page_content = qa_opt('closed_site_page_content') ? qa_opt('closed_site_page_content') : qa_lang('qa_closed_lang/default_page_content');
-			
-			$this->output('<div class="qa-closed-left">');
-			$this->output('<h1 class="closed-page-title">'.$page_title.'</h1>');
-			
-			$this->output(qa_opt('closed_site_html_left1'), '<!-- Html 1 Left -->');
-			$this->output('<div class="closed-page-content">'.$page_content.'</div>');
-			$this->output(qa_opt('closed_site_html_left2'), '<!-- Html 2 Left -->');
-			
-			$this->output('</div>');
-		}
-		
-		function qa_closed_left($login)
-		{
-			$login_title = qa_opt('closed-site-login-title') ? qa_opt('closed_site_login_title') : qa_lang('qa_closed_lang/default_login_title');
-			$login_content = qa_opt('closed-site-login-content') ? qa_opt('closed_site_login_content') : qa_lang('qa_closed_lang/default_login_content');
-			$this->output('<div class="qa-closed-right-items">');
-			$this->output('<span class="closed-page-title">'.$login_title.'</span><br><div class="closed_page_content_l">'.$login_content.'</div>');
-			
-			if (isset($this->content['error'])) $this->error($this->content['error']);
-			$this->output(qa_opt('closed_site_html_right1'), '<!-- Html 1 Right -->');
-			$this->output(
-				'<form action="' . $login['url'] . '" method="post">',
-					'<input type="text" name="emailhandle" dir="auto" placeholder="' . trim(qa_lang_html(qa_opt('allow_login_email_only') ? 'users/email_label' : 'users/email_handle_label'), ':') . '"/>',
-					'<input type="password" name="password" dir="auto" placeholder="' . trim(qa_lang_html('users/password_label'), ':') . '"/>',
-					'<div><input type="checkbox" name="remember" id="qam-rememberme" value="1"/>',
-					'<label for="qam-rememberme">' . qa_lang_html('users/remember') . '</label></div>',
-					'<input type="hidden" name="code" value="' . qa_html(qa_get_form_security_code('login')) . '"/>',
-					'<input type="submit" value="' . $login['label'] . '" class="qa-form-tall-button qa-form-tall-button-login" name="dologin"/>',
-				'</form><br>'
-			);
-			$this->output('<a class="qa-form-tall-button qa-form-tall-button-login" href="register">Register</a>');
-			$this->output('<br><br>');
-			
-			$this->output(qa_opt('closed_site_html_right2'), '<!-- Html 2 Right -->');			
-			$this->output('</div>');
-		}
-		
+		if(count($navigation) > 1 ) qa_html_theme_base::nav_list($navigation, $class, $level=null);	
 	}
-?>
+	
+	function head_css() {
+		parent::head_css();
+		if ($this->template=='closedsite'){
+			
+			$this->output(
+				'<script type="text/javascript">
+			var qa_wysiwyg_editor_config = {
+				defaultLanguage: \'en\',
+				language: \'\'
+			};
+				var qa_oldonload = window.onload;
+				window.onload = function() {
+				if (typeof qa_oldonload == \'function\')
+					qa_oldonload();
+
+				if (qa_ckeditor_closed_site_page_content_field = CKEDITOR.replace(\'closed_site_page_content_field\', qa_wysiwyg_editor_config)) { qa_ckeditor_closed_site_page_content_field.setData(document.getElementById(\'closed_site_page_content_field_ckeditor_data\').value); document.getElementById(\'closed_site_page_content_field_ckeditor_ok\').value = 1; }
+			};
+		</script>'
+			);
+			
+			$this->output('<script src="../qa-content/qa-admin.js?1.7.4"></script>');
+			$this->output('<script src="../qa-plugin/wysiwyg-editor/ckeditor/ckeditor.js?1.7.4"></script>');
+		}
+	}
+	
+	function doctype(){
+		global $qa_request;
+			
+		if ( ($qa_request == 'admin/closedsite') and (qa_get_logged_in_level()>=QA_USER_LEVEL_ADMIN) ) {			
+			$this->template="closedsite";
+			$this->content['navigation']['sub'] = qa_admin_sub_navigation();
+			$this->content['navigation']['sub']['closedsite'] = array(
+				'label' => qa_lang('qa_closed_lang/closed_site'),  
+				'url' => qa_path_html('admin/closedsite'),  
+				'selected' => 'selected'); 
+			
+			$this->content['error']="";
+			$this->content['suggest_next']="";
+			$this->content['title']= qa_lang_html('admin/admin_title').' - '.qa_lang('qa_closed_lang/closed_site_title');
+			
+			$saved = qa_clicked('closed_site_save');
+			$reset = qa_clicked('closed_site_reset');
+			if ($saved) {
+				qa_opt('closed_site_allow', (bool)qa_post_text('closed_site_allow_field'));
+				qa_opt('closed_site_page_title', qa_post_text('closed_site_page_title_field'));
+				qa_opt('closed_site_page_content', qa_post_text('closed_site_page_content_field'));
+				qa_opt('closed_site_login_title', qa_post_text('closed_site_login_title_field'));
+				qa_opt('closed_site_login_content', qa_post_text('closed_site_login_content_field'));
+				qa_opt('closed_site_background_allow', (bool)qa_post_text('closed_site_background_allow_field'));
+				qa_opt('closed_site_background_url', qa_post_text('closed_site_background_url_field'));
+				qa_opt('closed_site_html_top', qa_post_text('closed_site_html_top_field'));
+				qa_opt('closed_site_html_left1', qa_post_text('closed_site_html_left1_field'));
+				qa_opt('closed_site_html_left2', qa_post_text('closed_site_html_left2_field'));
+				qa_opt('closed_site_html_right1', qa_post_text('closed_site_html_right1_field'));
+				qa_opt('closed_site_html_right2', qa_post_text('closed_site_html_right2_field'));
+				qa_opt('closed_site_html_bottom', qa_post_text('closed_site_html_bottom_field'));
+			}
+			
+			if ($reset) {
+				qa_opt('closed_site_allow', '');
+				qa_opt('closed_site_page_title', '');
+				qa_opt('closed_site_page_content', '');
+				qa_opt('closed_site_login_title', '');
+				qa_opt('closed_site_login_content', '');
+				qa_opt('closed_site_background_allow', '');
+				qa_opt('closed_site_background_url', '');
+				qa_opt('closed_site_html_top', '');
+				qa_opt('closed_site_html_left1', '');
+				qa_opt('closed_site_html_left2', '');
+				qa_opt('closed_site_html_right1', '');
+				qa_opt('closed_site_html_right2', '');
+				qa_opt('closed_site_html_bottom', '');
+			}
+			
+			$options= array(
+				'ok' => $saved ? qa_lang('qa_closed_lang/closed_settings_saved') : null,
+				'tags' => 'method="post" action="'.qa_path_html(qa_request()).'"',
+				'style' => 'tall', 
+				'fields' => array(
+				
+				array(
+					'type' => 'checkbox',
+					'label' => qa_lang('qa_closed_lang/closed_site_allow'),
+					'value' => qa_opt('closed_site_allow'),
+					'tags' => 'name="closed_site_allow_field" id="closed_site_allow_field"',				
+				),
+			
+				array(
+					'type' => 'input',
+					'label' => qa_lang('qa_closed_lang/closed_site_page_title'),
+					'value' => qa_opt('closed_site_page_title'),
+					'tags' => 'name="closed_site_page_title_field" id="closed_site_page_title_field"',				
+				),
+				array(
+					'type' => 'textarea',
+					'label' => qa_lang('qa_closed_lang/closed_site_page_content'),
+					'value' => qa_opt('closed_site_page_content'),
+					'rows' => 7,
+					'tags' => 'name="closed_site_page_content_field" id="closed_site_page_content_field"',				
+				),
+			
+				array(
+					'type' => 'input',
+					'label' => qa_lang('qa_closed_lang/closed_site_login_title'),
+					'value' => qa_opt('closed_site_login_title'),
+					'tags' => 'name="closed_site_login_title_field" id="closed_site_login_title_field"',				
+				),
+				
+				array(
+					'type' => 'input',
+					'label' => qa_lang('qa_closed_lang/closed_site_login_content'),
+					'value' => qa_opt('closed_site_login_content'),
+					'tags' => 'name="closed_site_login_content_field" id="closed_site_login_content_field"',				
+				),
+				
+				array(
+					'type' => 'checkbox',
+					'label' => qa_lang('qa_closed_lang/closed_site_background_allow'),
+					'value' => qa_opt('closed_site_background_allow'),
+					'tags' => 'name="closed_site_background_allow_field" id="closed_site_background_allow_field"',				
+				),
+			
+				array(
+					'type' => 'input',
+					'label' => qa_lang('qa_closed_lang/closed_site_background_url'),
+					'value' => qa_opt('closed_site_background_url'),
+					'tags' => 'name="closed_site_background_url_field" id="closed_site_background_url_field"',				
+				),
+				array(
+					'type' => 'textarea',
+					'label' => qa_lang('qa_closed_lang/closed_site_html_top'),
+					'value' => qa_opt('closed_site_html_top'),
+					'rows' => 7,
+					'tags' => 'name="closed_site_html_top_field" id="closed_site_html_top_field"',				
+				),
+				array(
+					'type' => 'textarea',
+					'label' => qa_lang('qa_closed_lang/closed_site_html_left1'),
+					'value' => qa_opt('closed_site_html_left1'),
+					'rows' => 7,
+					'tags' => 'name="closed_site_html_left1_field" id="closed_site_html_left1_field"',				
+				),
+				array(
+					'type' => 'textarea',
+					'label' => qa_lang('qa_closed_lang/closed_site_html_left2'),
+					'value' => qa_opt('closed_site_html_left2'),
+					'rows' => 7,
+					'tags' => 'name="closed_site_html_left2_field" id="closed_site_html_left2_field"',				
+				),
+				
+				array(
+					'type' => 'textarea',
+					'label' => qa_lang('qa_closed_lang/closed_site_html_right1'),
+					'value' => qa_opt('closed_site_html_right1'),
+					'rows' => 7,
+					'tags' => 'name="closed_site_html_right1_field" id="closed_site_html_right1_field"',				
+				),
+				array(
+					'type' => 'textarea',
+					'label' => qa_lang('qa_closed_lang/closed_site_html_right2'),
+					'value' => qa_opt('closed_site_html_right2'),
+					'rows' => 7,
+					'tags' => 'name="closed_site_html_right2_field" id="closed_site_html_right2_field"',				
+				),
+				array(
+					'type' => 'textarea',
+					'label' => qa_lang('qa_closed_lang/closed_site_html_bottom'),
+					'value' => qa_opt('closed_site_html_bottom'),
+					'rows' => 7,
+					'tags' => 'name="closed_site_html_bottom_field" id="closed_site_html_bottom_field"',				
+				),
+				
+			),
+				
+				'buttons' => array(
+					array(
+						'label' => qa_lang('qa_closed_lang/save_changes'),
+						'tags' => 'name="closed_site_save"',
+					),
+					
+					array(
+						'label' => qa_lang('qa_closed_lang/reset_changes'),
+						'tags' =>   'name="closed_site_reset" onclick="return confirm('.qa_js(qa_lang_html('admin/reset_options_confirm')).');"',
+					),
+					
+				),
+			);
+			$this->content['form']=$options;
+			$this->content['custom']= '<p>If you think this plugin is great and helps you on your site please donate some $5 to $30 to my paypal account: <a href="mailto:smataweb@gmail.com">smataweb@gmail.com</a></p>';
+		}
+		qa_html_theme_base::doctype();
+	}
+}
