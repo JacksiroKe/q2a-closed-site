e916cc99fa42e707b2346debf117c6c01bc6f519
Jackson Siro <smataweb@gmail.com>
2017-04-18 10:10:59 +0300

    Update qa-closed-lang-default.php

--- closed-site/qa-closed-lang-default.php
+++ closed-site/qa-closed-lang-default.php
@@ -1,7 +1,7 @@
 <?php
 /*
 	Plugin Name: Closed Site
-	Plugin URI: http://github.com/JackSiro/Closed-Site-Plugin
+	Plugin URI: https://github.com/JackSiro/Q2A-Closed-Site-Plugin/
 	Plugin Description: Allows for for locking the site to only logged users only. Premium plugin coming out soon
 	Plugin Version: 1.5
 	Plugin Date: 2014-09-20
@@ -14,19 +14,25 @@
 */
 
 	return array(
-		// General
+		'closed_settings_saved' => 'Closed Site Settings saved!',
 		'closed_site' => 'Closed Site',
+		'closed_site_backgound' => 'url of background image',
+		'closed_site_background_allow' => 'Allow Background image',
+		'closed_site_background_url' => 'Background image url:',
 		'closed_site_allow' => 'Allow site to be closed',
 		'closed_site_allow_explanation' => 'Use the new link on your admin navigation to edit Closed site plugin settings <a href="admin/closedsite">or click here</a>',
-		'default_login_title' => 'Welcome to this site',
-		'default_login_content' => 'Our site is a unique site that lets you ask question and get answers from other members of the community.<br>Please login or sign up now to continue to our very wonderful site.',
-		'save_changes' => 'Save Changes',
-		
-		// Admin page
-		'closed_site_title' => 'Closed Site Settings',
-		'closed_site_page_title' => 'Login Page title:',
+		'closed_site_login_content' => 'Login Box Content (optional):',
+		'closed_site_login_title' => 'Login Box Title (optional):',
 		'closed_site_page_content' => 'Login Page content (HTML allowed):',
-		'closed_settings_saved' => 'Closed Site Settings saved!',
+		'closed_site_page_title' => 'Login Page title:',
+		'closed_site_title' => 'Closed Site Settings',
+		'default_login_content' => 'Login to your account now',
+		'default_login_title' => 'Login Now',
+		'default_page_title' => 'Welcome to this site',
+		'default_page_title' => 'Welcome to this site',
+		'default_page_content' => 'Our site is a unique site that lets you ask question and get answers from other members of the community.<br>Please login or sign up now to continue to our very wonderful site.',
+		'reset_changes' => 'Reset to Defaults',
+		'save_changes' => 'Save Changes',
 		
 	);
 
